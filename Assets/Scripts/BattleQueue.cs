c79940259d79515ba1e5efa879e728372e6c3f9f
Trainfire <aarongarcha@gmail.com>
2016-11-30 20:14:03 +0100

    Refactor queue.

--- Assets/Scripts/BattleQueue.cs
+++ Assets/Scripts/BattleQueue.cs
@@ -3,6 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+public enum BattleQueueType
+{
+    Normal,
+    PlayerCommand,
+    StatusUpdate,
+    Weather,
+}
+
 public class BattleQueue : MonoBehaviour
 {
     [SerializeField] private bool _logRegistrations;
@@ -56,43 +64,53 @@ public class BattleQueue : MonoBehaviour
         _weather.Clear();
     }
 
-    public void RegisterWeather(BaseAction action)
+    void RegisterAction(BaseAction action, BattleQueueType type)
     {
         if (_logRegistrations)
-            LogEx.Log<BattleQueue>("Registered weather: " + action.name);
+            LogEx.Log<BattleQueue>("Registered action: {0} of type '{1}'", action.GetType().Name, type.ToString());
 
-        _weather.Clear();
-        _weather.Enqueue(action);
+        switch (type)
+        {
+            case BattleQueueType.Normal:
+                _backLog.Enqueue(action);
+                break;
+            case BattleQueueType.PlayerCommand:
+                _playerCommands.Enqueue(action);
+                LogEx.Log<BattleQueue>("Registered player command '" + action.GetType() + "' targeting '" + (action as TargetedAction).Reciever.name + "'");
+                break;
+            case BattleQueueType.StatusUpdate:
+                _statusUpdates.Enqueue(action);
+                break;
+            case BattleQueueType.Weather:
+                _weather.Clear();
+                _weather.Enqueue(action);
+                break;
+        }
+    }
+
+    public void RegisterWeather(BaseAction action)
+    {
+        RegisterAction(action, BattleQueueType.Weather);
     }
 
     public void RegisterStatusUpdate(BaseAction action, string name)
     {
-        if (_logRegistrations)
-            LogEx.Log<BattleQueue>("Registered status update: " + name);
-
-        _statusUpdates.Enqueue(action);
+        RegisterAction(action, BattleQueueType.StatusUpdate);
     }
 
     public void RegisterAction(Action action, string name)
     {
-        if (_logRegistrations)
-            LogEx.Log<BattleQueue>("Registered action: " + name);
-
-        _backLog.Enqueue(AnonAction.Create(action, name));
+        RegisterAction(AnonAction.Create(action, name), BattleQueueType.Normal);
     }
 
     public void RegisterAction(BaseAction action)
     {
-        if (_logRegistrations)
-            LogEx.Log<BattleQueue>("Registered action: " + action.GetType().Name);
-
-        _backLog.Enqueue(action);
+        RegisterAction(action, BattleQueueType.Normal);
     }
 
     public void RegisterPlayerCommand(TargetedAction action)
     {
-        LogEx.Log<BattleQueue>("Registered player command '" + action.GetType() + "' targeting '" + action.Reciever.name + "'");
-        _playerCommands.Enqueue(action);
+        RegisterAction(action, BattleQueueType.PlayerCommand);
     }
 
     public bool Empty { get { return _current.Count == 0 && _master.Count == 0 && _backLog.Count == 0; } }
